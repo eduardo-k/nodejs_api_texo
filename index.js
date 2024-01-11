98b1b795aa7a6d321ddef1274d6adde239bbf599
Eduardo Kopper <ekopper@gmail.com>
2024-01-11 10:09:15 -0300

    fix: async start

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const app = express();
 const port = process.env.PORT || 3030;
 const routes = require('./app/routes');
 const initialization = require ('./scripts/initialization.js');
+const database = require('./database/db');
 
 (async () => {
-    const database = require('./database/db');
     try {
         await database.sync();
         await initialization.startDB();
+
+        app.use(express.json());
+        app.use(routes);
+        app.listen(port, () => {
+            console.log(`API started! Listening port ${port}!`)
+        });
     } catch (error) {
-        console.log(error);
-        throw new Error(error);
+        console.log(`API error at start: ${error.message}`);
+        throw new Error(error.message);
     }
 })();
-
-
-app.use(express.json());
-app.use(routes);
-app.listen(port, () => {
-    console.log(`API started, listening port ${port}!`)
-});
